e74c5f624bcff50b95ed33b32aca2fc244a8b44b
nico142857 <nicolas.toro.llanco@gmail.com>
2024-10-02 01:07:37 -0300

    now the code projects clusters and allows us to visualize NPP, T and CarbonExport

--- 02_codes/01_02_cluster_projection_map.py
+++ 02_codes/01_02_cluster_projection_map.py
@@ -6,6 +6,7 @@ import geopandas as gpd
 from mpl_toolkits.basemap import Basemap
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
+from matplotlib.colors import Normalize
 
 input_dir = '../03_results/out_genomic_clusters'
 filename = 'kmeans_results.tsv'
@@ -20,35 +21,94 @@ file_path = os.path.join(input_dir,filename)
 md_path = os.path.join(env_data_dir,env_filename)
 
 clusters = pd.read_csv(file_path, sep='\t', index_col=0)
+for col in clusters.columns:
+    clusters[col] = clusters[col].astype('Int64')
 md = pd.read_csv(md_path, sep='\t', index_col=0)
 
-merged_data = clusters.join(md[['Latitude', 'Longitude']])
+merged_data = clusters.join(md[['Latitude', 'Longitude', 'NPP 8d VGPM (mgC/m2/day)', 'Mean Flux at 150m', 'Temperature']])
 
 def plot_clusters_on_map(merged_data, cluster_column):
-    filtered_data = merged_data[~merged_data[cluster_column].isna()] # Filter nan values
+    filtered_data = merged_data[~merged_data[cluster_column].isna()] # filter out rows where cluster_column is NaN
     
-    fig = plt.figure(figsize=(12, 8))
-    ax = plt.axes(projection=ccrs.PlateCarree())
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20, 12), subplot_kw={'projection': ccrs.PlateCarree()})
+    axes = axes.flatten()  # flatten the array of axes for easy iteration
     
-    ax.add_feature(cfeature.LAND)
-    ax.add_feature(cfeature.OCEAN)
-    ax.add_feature(cfeature.COASTLINE)
-    #ax.add_feature(cfeature.BORDERS, linestyle=':')
-    ax.set_global()
+    plot_titles = [
+        f'Clusters Projection: {cluster_column}',
+        'NPP 8d VGPM (mgC/m2/day)',
+        'Mean Flux at 150m',
+        'Temperature'
+    ]
+    data_columns = [
+        cluster_column,
+        'NPP 8d VGPM (mgC/m2/day)',
+        'Mean Flux at 150m',
+        'Temperature'
+    ]
     
     unique_clusters = filtered_data[cluster_column].unique()
-    for cluster_id in unique_clusters:
-        cluster_data = filtered_data[filtered_data[cluster_column] == cluster_id]
-        plt.scatter(cluster_data['Longitude'], 
-                    cluster_data['Latitude'], 
-                    label=f'Cluster {cluster_id}',
-                    s=25, 
-                    transform=ccrs.PlateCarree())
+    num_clusters = len(unique_clusters)
+    marker_styles = ['o', 's', '^', 'v', '<', '>', 'd', 'p', 'h', 'H', '*', 'x', '+', 'D']
+    if num_clusters > len(marker_styles):
+        marker_styles = (marker_styles * ((num_clusters // len(marker_styles)) + 1))[:num_clusters]
+    cluster_marker_map = dict(zip(unique_clusters, marker_styles))
     
-    plt.title(f'Clusters Projection: {cluster_column}')
-    plt.legend(loc='upper left')
-    #plt.show()
-
+    env_vars = ['NPP 8d VGPM (mgC/m2/day)', 'Mean Flux at 150m', 'Temperature']
+    norms = {}
+    for data_column in env_vars:
+        vmin = filtered_data[data_column].min()
+        vmax = filtered_data[data_column].max()
+        norms[data_column] = Normalize(vmin=vmin, vmax=vmax)
+    
+    for idx, ax in enumerate(axes):
+        ax.add_feature(cfeature.LAND)
+        ax.add_feature(cfeature.OCEAN)
+        ax.add_feature(cfeature.COASTLINE)
+        ax.set_global()
+        ax.set_title(plot_titles[idx])
+        
+        data_column = data_columns[idx]
+        plot_data = filtered_data[~filtered_data[data_column].isna()]
+        
+        if idx == 0:
+            for cluster_id in unique_clusters:
+                cluster_points = plot_data[plot_data[cluster_column] == cluster_id]
+                ax.scatter(
+                    cluster_points['Longitude'],
+                    cluster_points['Latitude'],
+                    label=f'Cluster {cluster_id}',
+                    s=35,
+                    marker=cluster_marker_map[cluster_id],
+                    transform=ccrs.PlateCarree()
+                )
+            ax.legend(loc='upper left')
+        else:
+            # Other subplots: Environmental variables with cluster markers
+            norm = norms[data_column]
+            for cluster_id in unique_clusters:
+                cluster_points = plot_data[plot_data[cluster_column] == cluster_id]
+                sc = ax.scatter(
+                    cluster_points['Longitude'],
+                    cluster_points['Latitude'],
+                    c=cluster_points[data_column],
+                    s=35,
+                    cmap='viridis',
+                    marker=cluster_marker_map[cluster_id],
+                    edgecolors='black',
+                    norm=norm,
+                    transform=ccrs.PlateCarree()
+                )
+            cbar = plt.colorbar(sc, ax=ax, orientation='vertical', shrink=0.5)
+            cbar.set_label(data_column)
+            handles = []
+            for cluster_id in unique_clusters:
+                marker = cluster_marker_map[cluster_id]
+                handle = plt.Line2D([], [], color='black', marker=marker, linestyle='', markersize=8, label=f'Cluster {cluster_id}')
+                handles.append(handle)
+            ax.legend(handles=handles, loc='upper left')
+    
+    plt.tight_layout()
+    
     output_path = os.path.join(output_dir, f'clusters_{cluster_column}.pdf')
     plt.savefig(output_path, format='pdf', bbox_inches='tight')
     plt.close()
